995284434ac87dbb8dc9ba2c2ab19dc4fb27cd0c
DiogoKaster <diogokaster@gmail.com>
2024-03-29 19:37:40 -0300

    feat: create company class and refactor Main file code

---
+++ challenge/src/Empresa.java
@@ -0,0 +1,62 @@
+import cargos.Cargo;
+import cargos.Gerente;
+import cargos.Secretario;
+import cargos.Vendedor;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.text.Normalizer;
+
+public class Empresa {
+    private final Funcionario[] funcionarios;
+
+    public Empresa(JsonArray funcionariosJson) {
+        this.funcionarios = new Funcionario[funcionariosJson.size()];
+
+        if(!funcionariosJson.isEmpty()) {
+            for (int i = 0; i < funcionariosJson.size(); i++) {
+                JsonObject funcionario = funcionariosJson.get(i).getAsJsonObject();
+
+                String nomeFuncionario = funcionario.get("nome").getAsString();
+                String cargoFuncionario = Normalizer.normalize(funcionario.get("cargo").getAsString(), Normalizer.Form.NFD)
+                        .replaceAll("[^\\p{ASCII}]", "")
+                        .toLowerCase();
+                String contratacao = funcionario.get("contratacao").getAsString();
+
+                Funcionario funcionarioInstancia = getFuncionario(cargoFuncionario, nomeFuncionario, contratacao);
+                this.funcionarios[i] = funcionarioInstancia;
+            }
+        } else {
+            System.out.println("A lista para cadastro está vazia!");
+        }
+    }
+
+    private static Funcionario getFuncionario(String cargoFuncionario, String nomeFuncionario, String contratacao) {
+        Cargo cargo = null;
+
+        switch (cargoFuncionario) {
+            case "secretario":
+                cargo = new Secretario();
+                break;
+            case "vendedor":
+                cargo = new Vendedor();
+                break;
+            case "gerente":
+                cargo = new Gerente();
+                break;
+            default:
+                break;
+        }
+
+        return new Funcionario(nomeFuncionario, cargo, contratacao);
+    }
+
+    public void printaFuncionarios() {
+        for(Funcionario funcionario : this.funcionarios) {
+            System.out.println("Nome: " + funcionario.getNome());
+            System.out.println("Cargo: " + funcionario.getCargo());
+            System.out.println("Contratacao: " + funcionario.getInicioContrato());
+            System.out.println();
+        }
+    }
+}
--- challenge/src/Main.java
+++ challenge/src/Main.java
@@ -1,56 +1,12 @@
-import cargos.Cargo;
-import cargos.Gerente;
-import cargos.Secretario;
-import cargos.Vendedor;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-
-import java.text.Normalizer;
 
 public class Main {
     public static void main(String[] args) {
         Repository repositorio = new Repository();
+        JsonArray dataFuncionarios = repositorio.getFuncionarios();
+        JsonArray dataVendas = repositorio.getVendas();
 
-        JsonArray funcionarios = repositorio.getFuncionarios();
-        JsonArray vendas = repositorio.getVendas();
-
-        Cargo[] cargos = {
-                new Secretario(),
-                new Vendedor(),
-                new Gerente()
-        };
-
-        for (int i = 0; i < funcionarios.size(); i++) {
-            JsonObject funcionario = funcionarios.get(i).getAsJsonObject();
-
-            String nomeFuncionario = funcionario.get("nome").getAsString();
-            String cargoFuncionario = Normalizer.normalize(funcionario.get("cargo").getAsString(), Normalizer.Form.NFD)
-                    .replaceAll("[^\\p{ASCII}]", "")
-                    .toLowerCase();
-            String contratacao = funcionario.get("contratacao").getAsString();
-
-            Cargo cargo = null;
-
-            switch (cargoFuncionario) {
-                case "secretario":
-                    cargo = new Secretario();
-                    break;
-                case "vendedor":
-                    cargo = new Vendedor();
-                    break;
-                case "gerente":
-                    cargo = new Gerente();
-                    break;
-                default:
-                    break;
-            }
-
-            Funcionario funcionarioInstancia = new Funcionario(nomeFuncionario, cargo, contratacao);
-
-            System.out.println("Nome: " + funcionarioInstancia.getNome());
-            System.out.println("Cargo: " + funcionarioInstancia.getCargo());
-            System.out.println("Contratacao: " + funcionarioInstancia.getInicioContrato());
-            System.out.println();
-        }
+        Empresa empresa = new Empresa(dataFuncionarios);
+        empresa.printaFuncionarios();
     }
 }
